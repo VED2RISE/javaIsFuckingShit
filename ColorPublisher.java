f6d769aafb169a3190ef378804561283258f4a1b
Ivan Smirnov <smirnovi@cardiff.ac.uk>
2024-05-12 15:40:40 +0100

    multithreading

--- ColorPublisher.java
+++ ColorPublisher.java
@@ -1,14 +1,28 @@
-// TODO: Add code to this file as necessary
-//       to finish the implementation of ColorPublisher.
-
 import java.awt.Color;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class ColorPublisher
-{
-    public void addSubscriber(ColorSubscriber cs)
-    {
+public class ColorPublisher {
+    private final List<ColorSubscriber> subscribers = new CopyOnWriteArrayList<>();
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public void addSubscriber(ColorSubscriber cs) {
+        subscribers.add(cs);
     }
-    public void publish(Color c)
-    {
+
+    public void publish(Color c) {
+        executor.submit(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            for (ColorSubscriber subscriber : subscribers) {
+                subscriber.notifyColorChange(c);
+            }
+        });
     }
 }
